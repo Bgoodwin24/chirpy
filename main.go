6cfaf5308d5ad5eacd78b334fa618288e7d8ef0c
Bgoodwin24 <bgoodwin24@gmail.com>
2025-01-29 22:37:23 -0800

    Add request metrics and reset endpoints
    - Created apiConfig struct with atomic counter for hits
    - Added middleware to track fileserver requests
    - Added /metrics endpoint to display hit count
    - Added /reset endpoint to zero the counter

--- main.go
+++ main.go
@@ -1,30 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+// Stores a count of website views
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	// Create new server/handlers
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
+	// Listen for requests
 	log.Printf("Serving files from: %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+// Writes the number of requests counted as plain text to http response
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+// Increments fileserverHits for every request
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
